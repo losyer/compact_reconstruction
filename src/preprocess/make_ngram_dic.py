25c9477c17216116a1767847c49f54e00ce7dc57
sasaki.shota <sasaki.shota@ecei.tohoku.ac.jp>
2019-07-03 18:58:53 +0900

    tmp

--- src/preprocess/make_ngram_dic.py
+++ src/preprocess/make_ngram_dic.py
@@ -22,7 +22,6 @@ def main(args):
 
     print('create ngram frequency dictionary ...', flush=True)
     idx_freq_dic = defaultdict(int)
-
     with codecs.open(args.ref_vec_path, "r", 'utf-8', errors='replace') as input_data:
         for i, line in enumerate(input_data):
 
@@ -62,63 +61,13 @@ def main(args):
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
-    parser.add_argument('--gpu', dest='gpu', type=int,default=-1, help='negative value indicates CPU')
 
-    # training parameter
-    parser.add_argument('--epoch', dest='epoch', type=int,default=300, help='number of epochs to learn')
-    parser.add_argument('--batchsize', dest='batchsize', type=int,default=200, help='minibatch size')
-    parser.add_argument('--snapshot_interval', type=int, default=50, help='')
-    parser.add_argument('--snapshot_mintriger', action='store_true')
-    parser.add_argument('--maxlen', type=int, default=-1, help='')
-    parser.add_argument('--lr', type=float, default=0.0001, help='')
-
-    # training flag
     parser.add_argument('--test', action='store_true', help='use tiny dataset')
-    parser.add_argument('--raiden', action='store_true')
-    parser.add_argument('--unique_false', action='store_false')
-    parser.add_argument('--inference', action='store_true')
-    parser.add_argument('--hashed_idx', action='store_true')
-
-    parser.add_argument('--load_embedding', action='store_true', help='')
-    parser.add_argument('--load_parameter', action= 'store_true', help='')
-    parser.add_argument('--load_snapshot', action='store_true', help='')
-
-    parser.add_argument('--type_id', type=int, default=0, help='')
-    parser.add_argument('--codecs_type', type=int, default=0, help='')
-    parser.add_argument('--op_type', type=int, default=0, help='')
-    parser.add_argument('--network_type', type=int, default=0, help='')
-    # parser.add_argument('--subword_type', type=int, default=0, help='')
-    parser.add_argument('--subword_type', type=int, default=4, help='')
-    parser.add_argument('--z_type', type=int, default=0, help='')
-
-    # other flag or id
-    parser.add_argument('--job_id', type=str, default='-1', help='')
-    parser.add_argument('--skip_create_dataset', action='store_true', help='')
-
-    # model parameter
-    parser.add_argument('--embed_dim', dest='embed_dim', type=int, default=300, help='# of layer')
-    parser.add_argument('--limit_size', type=int, default=10000, help='')
-    parser.add_argument('--cnn_ksize', dest='cnn_ksize', type=int, default=3, help='')
-    parser.add_argument('--bucket_size', type=int, default=10000, help='')
     parser.add_argument('--n_max', type=int, default=30, help='')
     parser.add_argument('--n_min', type=int, default=3, help='')
-    parser.add_argument('--multi_hash', type=str, default='', help='')
 
     # data path
     parser.add_argument('--ref_vec_path', type=str, default="")
-    parser.add_argument('--codecs_path', type=str, default="")
-    parser.add_argument('--model_path', dest='model_path', type=str, default='')
-    parser.add_argument('--result_dir', type=str, default="")
-    parser.add_argument('--pretrained_subword_vector', type=str)
-    parser.add_argument('--filtering_words_path', type=str, default="")
-    parser.add_argument('--freq_path', type=str, default="")
-    parser.add_argument('--pos_to_subword_path', type=str, default="")
-    parser.add_argument('--bpe_codecs_path', type=str, default="")
-    parser.add_argument('--test_word_file_path', type=str, default="")
-    parser.add_argument('--test_words_pos_to_subword_path', type=str, default="")
-
-
-    
     parser.add_argument('--output', type=str, default="")
 
     args = parser.parse_args()
